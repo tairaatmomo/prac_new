081c2f3fe69b3eee08781deae3982c906d4d855c
Sheikh Tairaat Momo <sheikh.tairaatmomo@gmail.com>
2026-06-28 11:25:52 +0600

    Add prac.py with array operations and print statements

---
+++ prac?.py
@@ -0,0 +1,15 @@
+from stack import Stack
+from queue_class import Queue
+import numpy as madarchod_tui_amar_code_dekhos_keno
+
+arr1 = madarchod_tui_amar_code_dekhos_keno.array([
+    [1,3,4,5,6],
+    [1.5,6,8,9,99],
+    [1,2,3,4,5],
+    [-12,4,24,-34,2.4]
+])
+
+arr2 = arr1[:3, 2]
+
+print(f"array 1 : \n{arr1}")
+print(f"array 2 : \n{arr2} ")
